3b20d0908a9c69b0414f0973dfd374c9a65cdd8d
laurendimitra <lauren.mooore@bluetruckstudios.co>
2017-11-30 16:39:12 -0500

    all but the tweets

--- keys.js
+++ keys.js
@@ -1,10 +1,10 @@
-console.log('this is loaded');
+// console.log('this is loaded');
 
 var twitterKeys = {
-  consumer_key: '6WRRiS5GE8yJ9sXqeAgBvW2fn',
-  consumer_secret: 'L0cE5U8wTTohytCAXWOycS2PtkydinYv34axvNCsmiDg1hDUfv',
-  access_token_key: '872964302562570240-GHl8oP5TjZF9XBcbfLM0E6VJUO9lvzN',
-  access_token_secret: 'zokVT9hzEvybvnmF4luHZcxp0w1jOEpBpJNUOQwnwt2Eg',
+  consumer_key: 'uvzVIceLdJ15vKKlENlleDwKP',
+  consumer_secret: 'OuIXcxqmzi0FKLzxk723mZn5fwyg6RePCjD2NYFQzcB5dI0Ign',
+  access_token_key: '872964302562570240-zsaqtO2ITR4m9ZlC7prn4PiqTLNaI9s',
+  access_token_secret: 'qSNeKm3GQAj5JN2bIzyoB5qfU0YrnjsuydOYPsEpX9WJM',
 }
 
 module.exports = twitterKeys;
